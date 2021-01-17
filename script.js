9350f9e5d96505d25d8051cd289ea216e26f4002
seyoungjoo <sellyjphoto@gmail.com>
2021-01-17 23:12:28 +0900

    new update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -20,24 +19,12 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove('open');
-  scrollIntoView(link);
-});
-
-// Handle click on "contact me" button on home
-const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', () => {
-  scrollIntoView('#work');
-});
-
-// Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
 });
 
 // Show "arrow up" button when scrolling down
+const homeHeight = home.getBoundingClientRect().height;
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 1.2) {
@@ -49,40 +36,5 @@ document.addEventListener('scroll', () => {
 
 // Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
-  scrollIntoView('#home');
+  scrollIntoView('.intro');
 });
-
-// Projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
-  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
-  }
-
-  // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
-  if (active != null) {
-    active.classList.remove('selected');
-  }
-  e.target.classList.add('selected');
-
-  projectContainer.classList.add('anim-out');
-  setTimeout(() => {
-    projects.forEach((project) => {
-      if (filter === '*' || project.dataset.type.includes(filter)) {
-        project.classList.remove('invisible');
-      } else {
-        project.classList.add('invisible');
-      }
-    });
-    projectContainer.classList.remove('anim-out');
-  }, 300);
-});
-
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
